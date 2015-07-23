2c87aa5ccd6beac0538f6bc0e05fe0b4b954c790
Rafael Khan <raf.khan.f@gmail.com>
2015-07-23 14:45:39 -0400

    Create server

--- clusternator.js
+++ clusternator.js
@@ -1,3 +1,24 @@
 'use strict';
 
 var R = require('ramda');
+
+function noop(){}
+
+function getCluster(config) {
+  console.log('Starting the clusternator');
+
+  if(!config.clusterName) {
+   throw 'Config requires cluter name';
+  }
+
+  // CHECK IF CLUSTER EXISTS
+  // STORE CLUSTER ID
+
+
+  return {
+    createTask: noop,
+    listTasks: noop
+  };
+}
+
+module.exports = getCluster;
---
+++ server.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var R = require('ramda');
+var koa = require('koa');
+var route = require('koa-route');
+var bodyParser = require('koa-bodyparser');
+
+var app = koa();
+
+
+// Every application exists as a "unit"
+// Units contain everything needed to track down the git commit that generated
+// an image, and the image itself
+function getUnitController() {
+  //var UNIT_TYPE_PR = 'PR';
+  //var UNIT_TYPE_SHA = 'SHA';
+  //var UNIT_TYPE_TAG = 'TAG';
+  //var UNIT_TYPE_BRANCH = 'BRANCH';
+
+  //var units = [
+    //{
+      //ref: '51',
+      //type: UNIT_TYPE_PR,
+      //sha: 'xyz',
+      //imageUrl: 'abc',
+      //imageTag: 'def',
+      //start: new Date()
+    //}
+  //];
+
+  //function addUnit(unit) {
+  //}
+
+  function* addUnitHandler() {
+    var _this = this;
+    var reqBody = _this.request.body;
+
+    /*
+     * We should expect the request body to look like the following:
+     *
+     * {
+     *   ref: 51 | 'tag-name' | 'branch-name'
+     *   type: 'PR' | 'SHA' | 'TAG' | 'BRANCH', // What does the ref mean?
+     *   sha: '03d5895ec17f0f0bc38c1e76ee49ab7e6b0937e1f2193e6ac8f016e73f7e1cf5' // it says 'rangle.io'
+     *   imageUrl: '<dockerhub-url>',
+     *   imageTag: 'pr51', // This is the unique identifier for your image (TODO standardize this)
+     * }
+     */
+
+    if(!reqBody.ref  ||
+       !reqBody.type ||
+       !reqBody.sha  ||
+       !reqBody.imageUrl ||
+       !reqBody.imageTag) {
+
+      // TODO standard error messages
+      throw 'Missing unit arguments';
+    }
+
+    var newUnit = R.clone(reqBody);
+    newUnit.startTime = new Date();
+
+    this.state.responseData = newUnit;
+  }
+
+  return {
+    pushUnit: addUnitHandler
+  };
+}
+
+
+
+function* globalErrorHandler(next) {
+  try {
+    yield next;
+    this.body = JSON.stringify(this.state.responseData);
+  } catch(e) {
+    //TODO handle the error
+    console.log('caught', e);
+  }
+}
+
+app.use(globalErrorHandler);
+
+app.use(bodyParser());
+
+var unitController = getUnitController();
+app.use(route.get('/', unitController.pushUnit));
+
+app.listen(3000);
