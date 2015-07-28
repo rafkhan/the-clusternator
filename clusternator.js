ada6eff2de3616805f3558fb5b059c34bd08edbe
Rafael Khan <raf.khan.f@gmail.com>
2015-07-28 12:51:34 -0400

    Create task definition manager and gulpfile

--- clusternator.js
+++ clusternator.js
@@ -2,32 +2,32 @@
 
 var AWS = require('aws-sdk');
 
-var EC2Manager = require('./lib/ec2Manager');
-var ClusterManager = require('./lib/clusterManager');
+//var EC2Manager = require('./lib/ec2Manager');
+//var ClusterManager = require('./lib/clusterManager');
 
 var AWS_REGION = 'us-east-1';
 AWS.config.region = AWS_REGION;
 
-var ec2 = new AWS.EC2();
-var ec2Manager = EC2Manager(ec2);
+//var ec2 = new AWS.EC2();
+//var ec2Manager = EC2Manager(ec2);
 
-var instanceConfig = {
-  clusterName: 'CLUSTERNATOR',
+//var instanceConfig = {
+  //clusterName: 'CLUSTERNATOR',
 
-  // API config is merged directly into the API call
-  apiConfig: {
-    KeyName: 'project-augury-test-1-ecs-ami-us-east-1',
-    // XXX ENSURE IDEMPOTENCY, this needs to be different for every request
-    ClientToken: '6' //(new Date()).valueOf().toString()
-  }
-};
+  //// API config is merged directly into the API call
+  //apiConfig: {
+    //KeyName: 'project-augury-test-1-ecs-ami-us-east-1',
+    //// XXX ENSURE IDEMPOTENCY, this needs to be different for every request
+    //ClientToken: '5' //(new Date()).valueOf().toString()
+  //}
+//};
 
-ec2Manager.createEC2Instance(instanceConfig)
-          .then(function(data) {
-                console.log(data);
-              }, function(err) {
-                console.log(err);
-              });
+//ec2Manager.createEC2Instance(instanceConfig)
+          //.then(function(data) {
+                //console.log(data);
+              //}, function(err) {
+                //console.log(err);
+              //});
 
 
 //var ecs = new AWS.ECS();
