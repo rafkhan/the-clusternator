6f816370d24c400b661cbb6b9af5f29cd54416da
Rafael Khan <raf.khan.f@gmail.com>
2015-08-10 15:34:21 -0400

    Build docker image from CLI

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,6 @@ gulp.task('clusternator:app:destroy', function(){});
 gulp.task('clusternator:ec2:new', cli.newEC2Instance(argv));
 
 gulp.task('clusternator:server:start', cli.startServer(argv));
+
+gulp.task('clusternator:ci:circle:build', cli.circleCIBuild(argv));
+
