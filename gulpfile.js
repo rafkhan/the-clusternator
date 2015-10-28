d667ca679f64801b6c0a5046624b91f5e3624c90
Michael J. Bennett <michael.bennett@rangle.io>
2015-10-28 10:39:10 -0400

    Adds gulp tasks for linting, unit testing (mocha/chai), and coverage (istanbul)

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,41 @@
 'use strict';
 
 var gulp = require('gulp'),
-babel = require('gulp-babel');
+babel = require('gulp-babel'),
+eslint = require('gulp-eslint'),
+mocha = require('gulp-mocha'),
+istanbul = require('gulp-istanbul');
+
+var jsPaths = ['src/**/*.js'],
+specPaths = ['spec/**/*.spec.js'];
 
 gulp.task('default', ['transpile']);
+gulp.task('test', ['test-unit']);
 
-gulp.task('transpile', function () {
-  return gulp.src('src/**/*.js').
-    pipe(babel()).
-    pipe(gulp.dest('lib'));
+gulp.task('transpile', function transpile() {
+  return gulp.src(jsPaths).
+  pipe(babel()).
+  pipe(gulp.dest('lib'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('src/**/*.js', ['transpile']);
+gulp.task('watch', function watch() {
+  gulp.watch(jsPaths, ['transpile']);
+});
+
+gulp.task('pre-test-unit', ['lint'], function preUnitTest() {
+  return gulp.src(jsPaths).pipe(istanbul()).
+  pipe(istanbul.hookRequire());
+});
+
+gulp.task('test-unit', ['pre-test-unit'], function testUnit() {
+    return gulp.src(specPaths).
+    pipe(mocha()).
+    pipe(istanbul.writeReports());
+});
+
+gulp.task('lint', function lint() {
+  return gulp.src(jsPaths.concat(specPaths)).
+  pipe(eslint()).
+  pipe(eslint.format()).
+  pipe(eslint.failAfterError());
 });
