9d8390fe6fa83118dec517539e29918c96b2eb26
Michael J. Bennett <michael.bennett@rangle.io>
2015-11-06 14:21:48 -0500

    Adds diagram stubs, and removes the bin/clusternator-es5.js, adds postinstall hooks to transpile

--- bin/clusternator.sh
+++ bin/clusternator.sh
@@ -8,6 +8,6 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-SCRIPT=/clusternatorCli-es5.js
+SCRIPT=/clusternatorCli.js
 
 node $DIR$SCRIPT $@
