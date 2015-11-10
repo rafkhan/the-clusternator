c42d6f025e39c5c4305846201afa040ba5ac17c3
Rafael Khan <raf.khan.f@gmail.com>
2015-11-10 11:56:18 -0500

    Load PRManager asynchronously in server

--- src/server/main.js
+++ src/server/main.js
@@ -1,13 +1,20 @@
 'use strict';
 
+const TEST_VPC = 'vpc-ab07b4cf';
+const TEST_ZONE = 'us-east-1';
+
+var q = require('q');
 var express = require('express');
 var bodyParser = require('body-parser');
+var aws = require('aws-sdk');
+
+var getPRManager = require('../prManager');
 
 var prHandler = require('./pullRequest');
 var pushHandler = require('./push');
 var loggers = require('./loggers');
 
-function getServer(config) {
+function createServer(prManager) {
   var app = express();
 
   function ping(req, res) {
@@ -28,10 +35,33 @@ function getServer(config) {
   return app;
 }
 
+// XXX
+// This function is here to force the PR manager to load asynchronously,
+// such that we can query for a VPC ID before starting (as opposed to
+// using a hardcoded one right now).
+//
+function loadPRManagerAsync(config) {
+  var ec2 = new aws.EC2(config.credentials);
+  var ecs = new aws.ECS(config.credentials);
+  var r53 = new aws.Route53(config.credentials);
+  var prm = getPRManager(ec2, ecs, r53, TEST_VPC, TEST_ZONE);
+  return q.resolve(prm);
+}
+
+function getServer(config) {
+  return loadPRManagerAsync(config)
+          .then(createServer, (err) => {
+            return q.reject(err);
+          });
+}
+
 function startServer(config) {
   // wtf
-  getServer(config).listen(config.port);
-  console.log('Clusternator listening on port', config.port)
+  getServer(config)
+    .then((server) => {
+      server.listen(config.port); 
+      console.log('Clusternator listening on port', config.port)
+    });
 }
 
 module.exports = {
