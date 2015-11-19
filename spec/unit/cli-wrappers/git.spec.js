f87ab1171ae904b4338f7f603f033db3ae077bb8
Michael J. Bennett <michael.bennett@rangle.io>
2015-11-19 14:18:45 -0500

    Adds cli-wrapper support for git clone, and git rev-parse HEAD

--- spec/unit/cli-wrappers/git.spec.js
+++ spec/unit/cli-wrappers/git.spec.js
@@ -17,4 +17,56 @@ describe('Test git CLI Wrapper', () => {
     git.__set__('spawn', require('child_process').spawn);
   });
 
+
+  it('shaHead should resolve if there are no exit errors', (done) => {
+    git.shaHead().then(() => {
+      C.check(done, () => {
+        expect(true).to.be.ok;
+      });
+    }, C.getFail(done));
+  });
+
+  it('clone should resolve if there are no exit errors', (done) => {
+    git.clone('some-repo').then(() => {
+      C.check(done, () => {
+        expect(true).to.be.ok;
+      });
+    }, C.getFail(done));
+  });
+
+  it('clonse should throw if not given a repo', () => {
+    expect(() => {
+      git.clone();
+    }).to.throw(TypeError);
+  });
+
+
+  describe('cases with exit code', () => {
+
+    beforeEach(() => {
+      var ms = mockSpawn(),
+        runner = ms.simple(1, '');
+
+      runner.stderr = 'test error';
+      ms.setDefault(runner);
+      git.__set__('spawn', ms);
+    });
+
+    it('shaHead should reject if there are exit errors', (done) => {
+      git.shaHead().then(C.getFail(done), (err) => {
+        C.check(done, () => {
+          expect(err instanceof Error).to.be.ok;
+        });
+      });
+    });
+
+    it('clone should reject if there are exit errors', (done) => {
+      git.clone('a-repo').then(C.getFail(done), (err) => {
+        C.check(done, () => {
+          expect(err instanceof Error).to.be.ok;
+        });
+      });
+    });
+  });
+
 });
--- src/cli-wrappers/git.js
+++ src/cli-wrappers/git.js
@@ -1,4 +1,82 @@
+'use strict';
+
 const COMMAND = 'git';
+const FLAG_REV_PARSE = 'rev-parse';
+const FLAG_HEAD = 'HEAD';
+const FLAG_CLONE = 'cone';
 
 var spawn = require('child_process').spawn,
   Q = require('q');
+
+/**
+ * @returns {Q.Promise}
+ */
+function shaHead() {
+  var d = Q.defer(),
+    git = spawn(COMMAND, [FLAG_REV_PARSE, FLAG_HEAD]),
+    error = '',
+    output = '';
+
+  git.stdout.on('data', (data) => {
+    output += data;
+  });
+
+  git.stderr.on('data', (data) => {
+    error += data;
+  });
+
+  git.on('close', (code) => {
+    if (error) {
+      d.reject(new Error(error));
+    } else if (+code) {
+      d.reject(new Error('git terminated with exit code: ' + code));
+    } else {
+      d.resolve(output.trim());
+    }
+  });
+
+  git.stdin.end();
+
+  return d.promise;
+}
+
+/**
+ * @param {string} repo
+ * @returns {Q.Promise}
+ */
+function clone(repo) {
+  if (!repo) {
+    throw new TypeError('git: clone requires a repository string');
+  }
+  var d = Q.defer(),
+    git = spawn(COMMAND, [FLAG_CLONE, repo]),
+    error = '',
+    output = '';
+
+  git.stdout.on('data', (data) => {
+    output += data;
+  });
+
+  git.stderr.on('data', (data) => {
+    error += data;
+  });
+
+  git.on('close', (code) => {
+    if (error) {
+      d.reject(new Error(error));
+    } else if (+code) {
+      d.reject(new Error('git terminated with exit code: ' + code));
+    } else {
+      d.resolve(output.trim());
+    }
+  });
+
+  git.stdin.end();
+
+  return d.promise;
+}
+
+module.exports = {
+  shaHead,
+  clone
+};
