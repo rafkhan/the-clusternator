0a790f76d1947729b2de8d6a90095278e2859d95
Michael J. Bennett <michael.bennett@rangle.io>
2015-11-23 11:32:53 -0500

    clusternator init now adds private array to gitignore

--- src/cli-wrappers/git.js
+++ src/cli-wrappers/git.js
@@ -4,9 +4,67 @@ const COMMAND = 'git';
 const FLAG_REV_PARSE = 'rev-parse';
 const FLAG_HEAD = 'HEAD';
 const FLAG_CLONE = 'cone';
+const GITIGNORE = '.gitignore';
+const NEWLINE = '\n';
+const UTF8 = 'utf8';
 
 var spawn = require('child_process').spawn,
-  Q = require('q');
+  fs = require('fs'),
+  Q = require('q'),
+  path = require('path'),
+  clusternatorJson = require('../clusternator-json');
+
+var writeFile = Q.nbind(fs.writeFile, fs),
+  readFile = Q.nbind(fs.readFile, fs);
+
+/**
+ * @returns {Q.Promise<string>}
+ */
+function gitIgnorePath() {
+  return clusternatorJson.findProjectRoot().then((root) => {
+    return path.normalize(root + path.sep + GITIGNORE);
+  });
+}
+/**
+ * @returns {Q.Promise<string[]>}
+ */
+function readGitIgnore() {
+  return gitIgnorePath().then((ignoreFile) => {
+    return readFile(ignoreFile, UTF8).then((file) => {
+      return file.split(NEWLINE);
+    });
+  });
+}
+
+/**
+ * @param {string} toIgnore
+ * @param {string[]} ignores
+ * @returns {boolean}
+ */
+function gitIgnoreHasItem(toIgnore, ignores) {
+  var found = false;
+  ignores.forEach((str) => {
+    if (str.indexOf(toIgnore) === 0) {
+      found = true;
+    }
+  });
+  return found;
+}
+
+function addToGitIgnore(toIgnore) {
+  return readGitIgnore().then((ignores) => {
+    var output;
+    if (gitIgnoreHasItem(toIgnore, ignores)) {
+      // item already exists
+      return;
+    }
+    return gitIgnorePath().then((ignoreFile) => {
+      ignores.push(toIgnore);
+      output = ignores.join(NEWLINE);
+      return writeFile(ignoreFile, output);
+    });
+  });
+}
 
 /**
  * @returns {Q.Promise}
@@ -77,6 +135,13 @@ function clone(repo) {
 }
 
 module.exports = {
+  GITIGNORE,
   shaHead,
-  clone
+  clone,
+  addToGitIgnore,
+  helpers: {
+    gitIgnorePath,
+    readGitIgnore,
+    gitIgnoreHasItem
+  }
 };
