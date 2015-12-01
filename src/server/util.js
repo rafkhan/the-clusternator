1d0cf1f8737e78c1a0d014aa75ec9578db449845
Rafael Khan <raf.khan.f@gmail.com>
2015-11-30 20:22:34 -0500

    add more logging

--- src/server/util.js
+++ src/server/util.js
@@ -1,6 +1,9 @@
 'use strict';
 
+var log = require('./loggers').logger;
+
 function sendError(res, status, message) {
+  log.error(status, message);
   res.status(status).send({error: message});
 }
 
