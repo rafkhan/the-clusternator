2497e508367aae21c019fc6787407fa1a3cea8ed
Michael J. Bennett <michael.bennett@rangle.io>
2015-12-08 10:24:19 -0500

    Slack integrated into JS

---
+++ src/cli-wrappers/slack.js
@@ -0,0 +1,29 @@
+
+var spawn = require('child_process').spawn,
+  path = require('path'),
+  config = require('../config')(),
+  Q = require('q');
+
+const COMMAND = path.join(__dirname, '..', '..', 'bin', 'slack-hook.sh');
+
+function message(message, channel) {
+  if (!message || !channel) {
+    throw new TypeError('Slack messages require a messge, and channel');
+  }
+  var d = Q.defer(),
+    sshKeygen = spawn(COMMAND, [message, channel, config.slackURI]);
+
+  sshKeygen.on('close', (code) => {
+    if (+code) {
+      d.reject(new Error('git terminated with exit code: ' + code));
+    } else {
+      d.resolve();
+    }
+  });
+
+  return d.promise;
+}
+
+module.exports = {
+  message
+};
