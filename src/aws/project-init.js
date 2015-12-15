d11d1e1322a4e500461a4207d72f9f25eae7b40c
Rafael Khan <raf.khan.f@gmail.com>
2015-12-15 15:21:37 -0500

    use env variables for AWS auth when not in config file

--- src/aws/project-init.js
+++ src/aws/project-init.js
@@ -4,11 +4,14 @@ var awsProjectManager = require('./projectManager');
  * @returns {Q.Promise}
  */
 function initAwsProject(config) {
+  
+  var creds = config.awsCredentials || undefined;
+
   var a = require('aws-sdk'),
-    ec2 = new a.EC2(config.awsCredentials),
-    ecs = new a.ECS(config.awsCredentials),
-    r53 = new a.Route53(config.awsCredentials),
-    ddb = new a.DynamoDB(config.awsCredentials);
+    ec2 = new a.EC2(creds),
+    ecs = new a.ECS(creds),
+    r53 = new a.Route53(creds),
+    ddb = new a.DynamoDB(creds);
 
   return awsProjectManager(ec2, ecs, r53, ddb);
 }
