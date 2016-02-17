2e1c2bfdbb3f6bcfb1d11dc2b2f9bd701e6b0068
Michael J. Bennett <michael.bennett@rangle.io>
2016-02-16 23:59:21 -0500

    Adds a new project interface around existing project code

--- src/server/db/projects.js
+++ src/server/db/projects.js
@@ -4,7 +4,6 @@
  *
  * @module server/db/projects
  */
-const TABLE_NAME = 'projects';
 const POLL_INTERVAL = 30000;
 const ENCRYPTED_PROPS = Object.freeze(['sharedKey', 'gitHubKey']);
 
@@ -14,6 +13,116 @@ const Q = require('q');
 const util = require('../../util');
 const crypto = require('../auth/crypto-symmetric');
 
+const pruneAndStringify = R.compose(JSON.stringify, pruneRecord);
+
+module.exports = getProjectsDB;
+module.exports.createAccessor = createAccessor;
+module.exports.pruneRecord = pruneRecord;
+module.exports.write = write;
+module.exports.read = read;
+module.exports.createMapEncrypt = createMapEncrypt;
+
+/**
+ * @param {function(string, *=)} hashTable
+ * @param {string} encryptionKey
+ * @returns {accessor}
+ */
+function createAccessor(hashTable, encryptionKey) {
+  const encrypt = R.partial(crypto.encrypt, encryptionKey);
+  const decrypt = R.partial(crypto.decrypt, encryptionKey);
+
+  /**
+   * @param {string|{ id: string, repo: string}} key or optionally an object
+   with an id prop
+   * @param {{ id: string, repo: string }=} value
+   * @returns {function()}
+   */
+  function accessor(key, value) {
+    if (value === undefined) {
+      if (typeof key === 'object' && key && key.id){
+        return write(hashTable, encrypt, key.id, key);
+      }
+      return read(hashTable, decrypt, key);
+    }
+    return write(hashTable, encrypt, value.id, value);
+  }
+  return accessor;
+}
+
+/**
+ * @param {{ id: string, repo: string }} record
+ * @returns {{id: string, repo: string, name: (string), sharedKey: (string),
+ gitHubKey: (string), channel: string}}
+ * @throws {TypeError}
+ */
+function pruneRecord(record) {
+  const invalid = 'projects need at least an id, and repo';
+  if (!record) {
+    throw new TypeError(invalid);
+  }
+  if (!record.id || !record.repo) {
+    throw new TypeError(invalid);
+  }
+  return {
+    id: record.id,
+    repo: record.repo,
+    name: record.name || '',
+    sharedKey: record.sharedKey || '',
+    gitHubKey: record.gitHubKey || '',
+    channel: record.channel || record.id
+  };
+}
+
+/**
+ * @param {function(string, *=)} hashTable
+ * @param {function(string)} encryptionFunction
+ * @param {string} id
+ * @param {{ id: string, repo: string }} record
+ * @returns {function(...)}
+ */
+function write(hashTable, encryptionFunction, id, record) {
+  return hashTable(id, JSON.stringify(R
+    .mapObjIndexed(createMapEncrypt(encryptionFunction, ENCRYPTED_PROPS),
+      pruneRecord(record))));
+}
+
+/**
+ * @param {function(string, *)} hashTable
+ * @param {function(string)} decryptionFunction
+ * @param {string} id
+ * @returns {promiseToRead}
+ */
+function read(hashTable, decryptionFunction, id) {
+  /**
+   * @returns {Promise}
+   */
+  function promiseToRead() {
+    return hashTable(id)()
+      .then(JSON.parse)
+      .then(createMapEncrypt(decryptionFunction, ENCRYPTED_PROPS));
+  }
+  return promiseToRead;
+}
+
+/**
+ * @param {Function} encryptionFunction
+ * @param {Array<string>} encryptedProps
+ * @returns {mapEncrypt}
+ */
+function createMapEncrypt(encryptionFunction, encryptedProps) {
+  /**
+   * @param {*} val
+   * @param {string} key
+   * @returns {string}
+   */
+  function mapEncrypt(val, key) {
+    if (encryptedProps.indexOf(key) !== 0) {
+      return val;
+    }
+    return encryptionFunction(val);
+  }
+  return mapEncrypt;
+}
 
 /**
  * Returns an interface to a projectDb
@@ -116,9 +225,7 @@ function getProjectsDB(config, pm) {
   }
 
   function setItem(id, val) {
-    console.log('here', id, val);
     db[id] = R.mapObjIndexed(mapEncrypt, val);
-    console.log('not here');
     return Q.resolve(val);
   }
 
@@ -157,4 +264,3 @@ function getProjectsDB(config, pm) {
   };
 }
 
-module.exports = getProjectsDB;
--- src/server/db/projects.spec.js
+++ src/server/db/projects.spec.js
@@ -1,9 +1,120 @@
 'use strict';
 
 const rewire = require('rewire');
+const memory = require('./memory');
 const projectDb = rewire('./projects');
+const C = require('../../chai');
 
 /*global describe, it, expect, beforeEach, afterEach */
 describe('Projects DB', () => {
+  const identity = (i) => i;
+  let db;
+  let hashTable;
+
+  beforeEach(() => {
+    db = {};
+    hashTable = memory.bindDb(db).hashTable('t');
+  });
+
+  describe('createMapEncrypt', () => {
+    it('should return a function', () => {
+      expect(typeof projectDb.createMapEncrypt() === 'function').to.be.ok;
+    });
+    
+    it('should run the encryption function on props in props list', () => {
+      const mapEncrypt = projectDb.createMapEncrypt((x) => x + 5, ['a']);
+      expect(mapEncrypt(5, 'b') === 5).to.be.ok;
+    });
+
+    it('should not run the encryption function if prop is not in props list',
+      () => {
+        const mapEncrypt = projectDb.createMapEncrypt((x) => x + 5, ['a']);
+        expect(mapEncrypt(5, 'a') === 10).to.be.ok;
+    });
+  });
+
+  describe('read function', () => {
+    it('should return a function', () => {
+      expect(typeof projectDb.read(hashTable, identity) === 'function')
+        .to.be.ok;
+    });
+
+    it('should return a function that returns a promise', () => {
+      const p = projectDb.read(hashTable, identity)('test');
+      expect(typeof p.then === 'function').to.be.ok;
+    });
+  });
+
+  describe('write function', () => {
+    it('should return a function', () => {
+      expect(typeof projectDb.write(hashTable, identity, 'test', {
+          id: 'test',
+          repo: 'slash'
+        }) === 'function')
+        .to.be.ok;
+    });
+
+    it('\s returned function should return a write promise', (done) => {
+      db.t = {};
+      projectDb.write(hashTable, identity, 't', { id: 't', repo: 't'})()
+        .then((r) => C.check(done, () => expect(r).is.ok), C.getFail(done));
+    });
+  });
+
+  describe('createAccessor', () => {
+    it('should return a function', () => {
+      expect(typeof projectDb.createAccessor(hashTable, 'secret') ===
+        'function')
+        .to.be.ok;
+    });
+
+    it('\'s returned function should return a promise returning read ' +
+      'function when only given a key', (done) => {
+      db.t = {};
+      db.t.key = '{}'; // must be valid JSON
+      const accessor = projectDb.createAccessor(hashTable, 'secret');
+      accessor('key')()
+        .then((r) => C
+          .check(done, () => expect(JSON.stringify(r) === '{}').to.be.ok),
+          C.getFail(done));
+    });
+
+    it('\'s returned function should return a promise returning write ' +
+      'function when given a key and value', (done) => {
+      db.t = {};
+      const accessor = projectDb.createAccessor(hashTable, 'secret');
+      accessor('key', { id: 'key', repo: 't'})()
+        .then((r) => C
+          .check(done, () => expect(db.t.key).to.be.ok),
+          C.getFail(done));
+    });
+
+    it('\'s returned function should return a promise returning write ' +
+      'function when given only a value', (done) => {
+      db.t = {};
+      const accessor = projectDb.createAccessor(hashTable, 'secret');
+      accessor({ id: 'key', repo: 't'})()
+        .then((r) => C
+          .check(done, () => expect(db.t.key).to.be.ok),
+          C.getFail(done));
+    });
+  });
+
+  describe('pruneRecord function', () => {
+    it('should throw if not given a record', () => {
+      expect(() => projectDb.pruneRecord()).to.throw(Error);
+    });
+    it('should throw if not given a record.key', () => {
+      expect(() => projectDb.pruneRecord({ repo: 't' })).to.throw(Error);
+    });
+
+    it('should throw if not given a record.repo', () => {
+      expect(() => projectDb.pruneRecord({ id: 't' })).to.throw(Error);
+    });
+
+    it('should return an object', () => {
+      expect(projectDb.pruneRecord({ id: 't', repo: 't' }).id === 't').to.be.ok;
+    });
+  });
 
 });
