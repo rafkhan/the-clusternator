b21b5936626358ba0fdca41311faf07569864cec
Michael J. Bennett <michael.bennett@rangle.io>
2016-02-29 11:56:41 -0500

    Adds solution for DynamoDB TLS issue: https://github.com/aws/aws-sdk-js/issues/862

--- src/aws/project-init.js
+++ src/aws/project-init.js
@@ -7,6 +7,7 @@
 
 const awsProjectManager = require('./projectManager');
 const makePromiseApi = require('../util').makePromiseApi;
+const https = require('https');
 
 /**
  * @returns {Q.Promise}
@@ -18,7 +19,20 @@ function initAwsProject(config) {
   const ecr = new a.ECR(config.awsCredentials);
   const ecs = new a.ECS(config.awsCredentials);
   const r53 = new a.Route53(config.awsCredentials);
-  const ddb = makePromiseApi(new a.DynamoDB(config.awsCredentials));
+  /**
+   * This solution is sub-optimal unfortunately DDB servers seem to have only
+   * deprecated versions of TLS
+   * https://github.com/aws/aws-sdk-js/issues/862
+   */
+  const ddb = makePromiseApi(
+    new a.DynamoDB(Object.assign({
+      httpOptions: {
+        agent: new https.Agent({
+          secureProtocol: 'TLSv1_method',
+          ciphers: 'ALL'
+        })
+      }
+    }, config.awsCredentials)));
   const iam = new a.IAM(config.awsCredentials);
 
   return awsProjectManager(ec2, ecs, r53, ddb, iam, ecr, elb);
